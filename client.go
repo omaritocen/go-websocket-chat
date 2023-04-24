517e6c22d258b7c02d91f143fe401452f4f40e8e
omaritocen <omaralaaabdelkader@gmail.com>
2023-04-24 02:01:09 +0200

    crate initial client type/logic

--- client.go
+++ client.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -15,4 +19,36 @@ type Client struct {
 
 	// Buffered channel of outbound messages
 	send chan []byte
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+func (c *Client) readPump() {
+	// TODO: ...
+}
+
+// writePump pumps messages from the hub to the websocket connection.
+func (c *Client) writePump() {
+	// TODO: ...
+}
+
+func newClient(hub *Hub, conn *websocket.Conn) *Client {
+	return &Client{
+		hub:  hub,
+		conn: conn,
+		send: make(chan []byte, 256),
+	}
+}
+
+func serverWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := newClient(hub, conn)
+	hub.register <- client
+
+	go client.writePump()
+	go client.readPump()
 }
